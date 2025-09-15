6924c4b9357fb743eb80cf53aba7d3fc1b9780aa
GBBTECHIVE <gbtechive082@gmail..com>
2025-09-15 10:18:33 +0500

    next config file updated

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.vercel.com",
+        port: "",
+        pathname: "/image/upload/**",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+}
+
+module.exports = nextConfig
